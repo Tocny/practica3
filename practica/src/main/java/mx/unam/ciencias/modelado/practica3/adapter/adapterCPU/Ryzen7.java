ebc8377f90b31de58ccfd6135f364567419147c0
goldenemilly <adamleon@ciencias.unam.mx>
2024-09-21 21:36:31 -0600

    Actualizacion de Adapter para CPU

--- practica/src/main/java/mx/unam/ciencias/modelado/practica3/adapter/adapterCPU/Ryzen7.java
+++ practica/src/main/java/mx/unam/ciencias/modelado/practica3/adapter/adapterCPU/Ryzen7.java
@@ -1,1 +1,23 @@
 package mx.unam.ciencias.modelado.practica3.adapter.adapterCPU;
+
+class Ryzen7 {
+    public String getName() {
+        return "Ryzen 7";
+    }
+
+    public String getBrand() {
+        return "AMD";
+    }
+
+    public int getCores() {
+        return 8;
+    }
+
+    public double getCost() {
+        return 3700;
+    }
+
+    public String description() {
+        return getName() + " de " + getBrand() + " con " + getCores() + " cores.";
+    }
+}
